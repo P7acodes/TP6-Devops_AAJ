a905955edcf097aa7bf37bf6126d1283b9efc9c5
p7acodes <abdelhaq.ajnaki@dauphine.eu>
2023-11-27 19:17:24 +0000

    TP6 DevOps

--- main.tf
+++ main.tf
@@ -35,16 +35,6 @@ resource "google_sql_database" "wordpress" {
   name     = "wordpress"
   instance = "main-instance"
 }
-#resource "google_sql_database_instance" "main-instance" {
-#  name             = "main-instance"
-#  region           = "us-central1"
-#  database_version = "MYSQL_8_0"
-#  settings {
-#    tier = "db-f1-micro"
-#  }
-
- # deletion_protection  = "true"
-#}
 
 resource "google_sql_user" "wordpress" {
    name     = "wordpress"
